8b0a814f5f5fc984327e72003312043ee19eb46c
doneni <xxerrenn@icloud.com>
2023-09-29 23:37:48 +0900

    init_setup done

--- src/Init_Setup.java
+++ src/Init_Setup.java
@@ -1,14 +1,31 @@
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.SecureRandom;
+import java.util.Base64;
 
-public class Init_Setup {
-    public void H(String plainText)
+public class Init_Setup
+{
+    public static final int num = 10000;
+    public static final int len = 1000000; // (bytes) maximum length of files
+    public static final int cNum = 100;
+    public static final String iv_1 = generateRandomString(16);
+    public static final String iv_2 = generateRandomString(16);
+    private static final String AES_CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding";
+    private static final String key = "THIS_IS_AES_PRIVATE_KEY_FOR_TEST";
+    private static final String iv = key.substring(0, 16);
+
+    public String h(String plainText)
     {
         try
         {
             MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
             byte[] hash = sha256.digest(plainText.getBytes());
+//            System.out.println("hash" + hash.toString());
 
+//            return hash;
+            /* hex to hexString */
             StringBuilder hexString = new StringBuilder();
             for (byte b : hash)
             {
@@ -17,9 +34,68 @@ public class Init_Setup {
                     hexString.append('0');
                 hexString.append(hex);
             }
-            LoggerManager.logInfo("SHA-256 hash: " + hexString.toString());
-        } catch (NoSuchAlgorithmException e) {
+            LoggerManager.logInfo("[*] hash operation done");
+            return hexString.toString();
+        } catch (Exception e) {
             e.printStackTrace();
+            LoggerManager.logError("[-] hash operation failed", e);
         }
+        return null;
+    }
+
+    public String enc(String plainText)
+    {
+        try
+        {
+            Cipher cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
+
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(key.substring(0,16).getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes("UTF-8"));
+
+            LoggerManager.logInfo("[*] AES256 encryption done");
+
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            LoggerManager.logError("[*] AES256 encryption failed", e);
+        }
+        return null;
+    }
+
+    public String dec(String encryptedText)
+    {
+        try
+        {
+            Cipher cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
+
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+            byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+
+            LoggerManager.logInfo("[*] AES256 decryption done");
+
+            return new String(decryptedBytes, "UTF-8");
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            LoggerManager.logError("[*] AES256 decryption failed", e);
+        }
+        return null;
+    }
+
+    private static String generateRandomString(int len)
+    {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] randomBytes = new byte[len];
+        secureRandom.nextBytes(randomBytes);
+
+        return Base64.getEncoder().encodeToString(randomBytes);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,27 @@ public class Main
 {
     public static void main(String[] args)
     {
-        LoggerManager.logInfo("Hello");
         Init_Setup init = new Init_Setup();
-        init.H("Hello World");
-        LoggerManager.logInfo("[*] Init_Setup::H done...");
 
+//        String ret = init.h("Hello World");
+//        System.out.println("SHA256: " + ret);
+//        LoggerManager.logInfo("[*] Init_Setup::h done...");
 
+        /*
+        String encryptedText = init.enc("Hello World");
+        String decryptedText = init.dec(encryptedText);
+
+        System.out.println("Encrypted: " + encryptedText);
+        System.out.println("Decrypted: " + decryptedText);
+
+        System.out.println(init.iv_1);
+        System.out.println(init.iv_2);
+
+        System.out.println(init.len);
+        System.out.println(init.cNum);
+        System.out.println(init.num);
+         */
+
+        Client c = new Client();
     }
 }
